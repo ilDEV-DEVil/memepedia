66727fc106a6ba5ef214c141401941cf1ff6346f
Nimesh Palakuttige Fernando <84867482+flashbLack98@users.noreply.github.com>
2025-12-14 13:59:56 +0100

    fix: fixes test

--- src/app/pages/timeline/timeline.component.spec.ts
+++ src/app/pages/timeline/timeline.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TimelineComponent } from './timeline.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
 describe('TimelineComponent', () => {
     let component: TimelineComponent;
@@ -8,7 +9,11 @@ describe('TimelineComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [TimelineComponent]
+            imports: [TimelineComponent],
+            providers: [
+                provideHttpClient(),
+                provideRouter([])
+            ]
         })
             .compileComponents();
 
